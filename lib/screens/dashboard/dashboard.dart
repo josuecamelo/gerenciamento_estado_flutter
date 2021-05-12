82f6d5ad40218f6b6fdfe66cf9d9e1a568563b08
Josué Camelo <josueprg@gmail.com>
2021-05-11 23:22:08 -0300

    Multiplos Providers

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,6 +1,9 @@
 import 'package:bytebank/models/saldo.dart';
 import 'package:bytebank/screens/dashboard/saldo.dart';
+import 'package:bytebank/screens/deposito/formulario.dart';
+import 'package:bytebank/screens/transferencia/formulario.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -8,9 +11,34 @@ class Dashboard extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Bytebank'),
         ),
-        body: Align(
-          alignment: Alignment.topCenter,
-          child: SaldoCard(),
-        ));
+        body: ListView(children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: SaldoCard(),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                color: Colors.green,
+                child: Text('Receber Depósito'),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return FormularioDeposito();
+                  }));
+                },
+              ),
+              RaisedButton(
+                color: Colors.green,
+                child: Text('Nova Transferência'),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return FormularioTransferencia();
+                  }));
+                },
+              )
+            ],
+          )
+        ]));
   }
 }
